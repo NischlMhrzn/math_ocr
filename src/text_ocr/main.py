516b39e1663478776fcf50f066408f4b58cf2327
FM-PC-LT-140 <you@example.com>
2022-04-21 23:33:48 +0545

    [feat]: Add OCR for text

---
+++ src/text_ocr/main.py
@@ -0,0 +1,22 @@
+import pytesseract
+import cv2
+import argparse
+
+
+def ocr(image):
+    # configurations
+    config = "-l eng+equ --oem 1 --psm 6 tessconfig"
+    # pytessercat
+    text = pytesseract.image_to_string(image, config=config)
+    # print text
+    text = text.split("\n")
+    return text[:-1]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--img_path", type=str, default=None, help="path of image for OCR"
+    )
+    args = parser.parse_args()
+    print(ocr(args.img_path))
